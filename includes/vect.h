db4ca138d87ec81fafce24cda3eb49dbb861255a
Никита Иванов <cppivanov@gmail.com>
2021-01-10 15:33:57 +0300

    refactor + CMake

---
+++ includes/vect.h
@@ -0,0 +1,103 @@
+#ifndef VECT_H
+# define VECT_H
+
+# include <cmath>
+# include <cassert>
+#include <iostream>
+
+template <size_t DIM, typename T>
+struct vec
+{
+	vec()  { for (size_t i =  0; i < DIM; i++) data_[i] = T(); }
+    T &			operator[](const size_t i) noexcept { assert(i < DIM); return (data_[i]); }
+    T const &	operator[](const size_t i) const noexcept { assert(i < DIM); return (data_[i]); }
+
+private:
+	T	data_[DIM];
+};
+
+typedef vec<2, float>	Vec2f;
+typedef vec<2, int>		Vec2i;
+typedef vec<2, double>	Vec2d;
+typedef vec<3, float>	Vec3f;
+typedef vec<3, int>		Vec3i;
+typedef vec<3, double>	Vec3d;
+
+template <typename T>
+struct vec<2, T> {
+	vec() : x(T()), y(T()) { }
+	vec(T X, T Y) : x(X), y(Y) { }
+    T &			operator[](const size_t i) noexcept { assert(i < 2 && i >= 0); return (i == 0 ? x : y); }
+    T const &	operator[](const size_t i) const noexcept { assert(i < 2 && i >= 0); return (i == 0 ? x : y); }
+	T x, y;
+};
+
+template <typename T>
+struct vec<3, T>
+{
+	vec() : x(T()), y(T()), z(T()) { }
+	vec(T X, T Y, T Z) : x(X), y(Y), z(Z) { }
+    T &			operator[](const size_t i) noexcept { assert(i < 3 && i >= 0); return (i == 0 ? x : (i == 1 ? y : z)); }
+    T const &	operator[](const size_t i) const noexcept { assert(i < 3 && i >= 0); return (i == 0 ? x : (i == 1 ? y : z)); }
+    double      norm() const noexcept { return std::sqrt(x * x + y * y + z * z); }
+    vec<3, T> & normalize(T l = 1) { *this = (*this) * (l / norm()); return *this; }
+	T x, y, z;
+};
+
+template <size_t DIM, typename T>
+vec<DIM, T>     operator+(vec<DIM, T> const & lhs, vec<DIM, T> const & rhs) noexcept
+{
+    vec<DIM, T> ret;
+    for (size_t i = 0; i < DIM; i++)
+        ret[i] = lhs[i] + rhs[i];
+    return (ret);
+}
+
+template <size_t DIM, typename T>
+vec<DIM, T>     operator-(vec<DIM, T> const & lhs, vec<DIM, T> const & rhs) noexcept
+{
+    vec<DIM, T> ret;
+    for (size_t i = 0; i < DIM; i++)
+        ret[i] = lhs[i] - rhs[i];
+    return (ret);
+}
+
+template<size_t DIM, typename T>
+T               operator*(vec<DIM, T> const & lhs, vec<DIM, T> const & rhs)
+{
+    T ret = T();
+    for (size_t i = 0; i < DIM; i++)
+        ret += lhs[i] * rhs[i];
+    return (ret);
+}
+
+template<size_t DIM, typename T, typename U>
+vec<DIM, T>     operator*(vec<DIM, T> const & lhs, U const & rhs) noexcept
+{
+    vec<DIM, T> ret;
+    for (size_t i = 0; i < DIM; i++)
+        ret[i] = lhs[i] * rhs;
+    return (ret);
+}
+
+template<size_t DIM, typename T>
+vec<DIM, T>     operator-(vec<DIM, T> const & rhs) noexcept
+{
+    return (rhs * T(-1));
+}
+
+template<size_t DIM, typename T>
+std::ostream &  operator<<(std::ostream & out, vec<DIM, T> const & v)
+{
+    for (size_t i = 0; i < DIM; i++)
+        out << v[i] << ' ';
+    return (out);
+}
+
+template<typename T>
+vec<3, T>       cross(vec<3, T> const & v1, vec<3, T> const & v2)
+{
+    return (vec<3, T>(v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x));
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,76 @@
 #include <vector>
 #include <fstream>
 
-void    write_to_ppm(std::vector<Vec3d> framebuffer) {
-    std::ofstream   ofs;
-    ofs.open("./out.ppm");
-    ofs << "P6\n" << WIDTH << " " << HEIGHT << "\n255\n";
-    for(size_t i = 0; i < WIDTH * HEIGHT; i++)
-        for (size_t j = 0; j < 3; j++)
-            ofs << static_cast<char>(255 * std::max(0., std::min(1., framebuffer[i][j])));
+void    write_to_bmp(std::vector<Vec3d> framebuffer) {
+    // mimeType = "image/bmp";
+    std::ofstream stream;
+    stream.open("out.bmp");
+
+    unsigned char file[14] = {
+        'B','M', // magic
+        0,0,0,0, // size in bytes
+        0,0, // app data
+        0,0, // app data
+        40 + 14,0,0,0 // start of data offset
+    };
+    unsigned char info[40] = {
+        40,0,0,0, // info hd size
+        0,0,0,0, // width
+        0,0,0,0, // heigth
+        1,0, // number color planes
+        24,0, // bits per pixel
+        0,0,0,0, // compression is none
+        0,0,0,0, // image bits size
+        0,0,0,0,
+        0,0,0,0,
+        //0x13,0x0B,0,0, // horz resoluition in pixel / m
+        //0x13,0x0B,0,0, // vert resolutions (0x03C3 = 96 dpi, 0x0B13 = 72 dpi)
+        0,0,0,0, // #colors in pallete
+        0,0,0,0, // #important colors
+    };
+
+    int w = WIDTH;
+    int h = HEIGHT;
+
+    //int padSize = (4 - 3 * w % 4) % 4;
+    int padSize = (4 - (w * 3) % 4) % 4;
+    int sizeData = w * h * 3 + h * padSize;
+    int sizeAll = sizeData + sizeof(file) + sizeof(info);
+
+    file[2] = (unsigned char)(sizeAll);
+    file[3] = (unsigned char)(sizeAll >> 8);
+    file[4] = (unsigned char)(sizeAll >> 16);
+    file[5] = (unsigned char)(sizeAll >> 24);
+
+    info[4] = (unsigned char)(w);
+    info[5] = (unsigned char)(w >> 8);
+    info[6] = (unsigned char)(w >> 16);
+    info[7] = (unsigned char)(w >> 24);
+
+    info[8] = (unsigned char)(h);
+    info[9] = (unsigned char)(h >> 8);
+    info[10] = (unsigned char)(h >> 16);
+    info[11] = (unsigned char)(h >> 24);
+
+    info[20] = (unsigned char)(sizeData);
+    info[21] = (unsigned char)(sizeData >> 8);
+    info[22] = (unsigned char)(sizeData >> 16);
+    info[23] = (unsigned char)(sizeData >> 24);
+
+    stream.write((char*)file, sizeof(file));
+    stream.write((char*)info, sizeof(info));
+
+    unsigned char pad[3] = { 0,0,0 };
+
+    for (int i = 0; i < h * w; i++)
+    {
+        unsigned char pixel[3];
+        pixel[0] = 255 * std::max(0., std::min(1., framebuffer[i][0]));
+        pixel[1] = 255 * std::max(0., std::min(1., framebuffer[i][1]));
+        pixel[2] = 255 * std::max(0., std::min(1., framebuffer[i][2]));
+
+        stream.write((char*)pixel, 3);
+    }
 }
 
 Vec3d   reflect(Vec3d const & I, Vec3d const & N) {
@@ -39,7 +102,8 @@ Vec3d   cast_ray(Vec3d const & orig, Vec3d const & dir, std::vector<Shape *> con
     Material    material;
 
     if (depth > 4 || !scene_intersect(orig, dir, figures, hit, N, material))
-        return (Vec3d(0.2, 0.7, 0.8));
+        return (Vec3d(0.0, 1.0, 0.0));
+        //return (Vec3d(0.2, 0.7, 0.8));
 
     Vec3d   reflect_dir = reflect(dir, N).normalize();
     Vec3d   reflect_orig = reflect_dir * N < 0 ?  hit - N * 1e-3 : hit + N * 1e-3;
@@ -47,7 +111,7 @@ Vec3d   cast_ray(Vec3d const & orig, Vec3d const & dir, std::vector<Shape *> con
 
     double  light_intensity = 0;
     double  spec_light_intensity = 0;
-    for (auto it : lights) {
+    for (auto& it : lights) {
         Vec3d   light_dir = (it.position - hit).normalize();
         double  light_dist = (it.position - hit).norm();
         Vec3d   shadow_orig = light_dir * N < 0 ? hit - N * 1e-3 : hit + N * 1e-3;
@@ -58,6 +122,7 @@ Vec3d   cast_ray(Vec3d const & orig, Vec3d const & dir, std::vector<Shape *> con
         light_intensity += it.intensity * std::max(0., light_dir * N);
         spec_light_intensity += pow(std::max(0., -reflect(-light_dir, N) * dir), material.spec_exp) * it.intensity;
     }
+    //return Vec3d(10.0, 0.0, -10.0);
     return material.color * light_intensity * material.albedo[0] + Vec3d(1., 1., 1.) * spec_light_intensity * material.albedo[1] + reflect_color * material.albedo[2];
 }
 
@@ -66,13 +131,16 @@ void    render(std::vector<Shape *> const & figures, std::vector<Light> const & 
     std::vector<Vec3d> framebuffer(WIDTH * HEIGHT);
     for (size_t j = 0; j < HEIGHT; j++) { // actual rendering loop
         for (size_t i = 0; i < WIDTH; i++) {
-            double  x =  (2 * (i + 0.5) / static_cast<double>(WIDTH)  - 1) * tan(FOV/2.) * WIDTH / static_cast<double>(HEIGHT);
-            double  y = -(2 * (j + 0.5) / static_cast<double>(HEIGHT) - 1) * tan(FOV / 2.);
-            Vec3d   dir = Vec3d(x, y, -1).normalize();
-            framebuffer[ i + j * WIDTH] = cast_ray(Vec3d(0,0,0), dir, figures, lights);
+            //double  x =  (2 * (i + 0.5) / static_cast<double>(WIDTH)  - 1) * tan(FOV/2.) * WIDTH / static_cast<double>(HEIGHT);
+            //double  y = -(2 * (j + 0.5) / static_cast<double>(HEIGHT) - 1) * tan(FOV / 2.);
+            //Vec3d   dir = Vec3d(x, y, -1).normalize();
+            double dir_x = (i + 0.5) - WIDTH / 2.;
+            double dir_y = -(j + 0.5) + HEIGHT / 2.;    // this flips the image at the same time
+            double dir_z = -HEIGHT / (2. * std::tan(FOV / 2.));
+            framebuffer[i + j * WIDTH] = cast_ray(Vec3d(0, 0, 0), Vec3d(dir_x, dir_y, dir_z).normalize(), figures, lights);
         }
     }
-    write_to_ppm(framebuffer);
+    write_to_bmp(framebuffer);
 }
 
 int		main()
